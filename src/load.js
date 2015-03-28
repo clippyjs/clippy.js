e9dce28efac4066b3fb4d5378e18ce6e61a0e006
Hesky Fisher <hesky.fisher@gmail.com>
2015-03-28 15:05:52 -0400

    load agents from local source rather than AWS

--- src/load.js
+++ src/load.js
@@ -1,4 +1,4 @@
-clippy.BASE_PATH = '//s3.amazonaws.com/clippy.js/Agents/';
+clippy.BASE_PATH = '../agents/';
 
 clippy.load = function (name, successCb, failCb) {
     var path = clippy.BASE_PATH + name;
